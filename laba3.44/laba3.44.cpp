bab3d268562812b56c4b467c8d3236f4982714cb
Teks <Teks@TEKS>
2023-09-22 00:26:09 +0700

    Добавьте файлы проекта.

---
+++ laba3.44/laba3.44.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	int a; cout << "Укажите балл в 5-ти бальной системе: "; cin >> a;
+	if (a < 0) {
+		cerr << "Ошибка: оценка должна быть больше нуля" <<
+			endl;
+		return 1;
+	}
+	switch (a) {
+	case 0: cout << "Хуже среднего" << endl; break;
+	case 1: cout << "Хуже среднего" << endl; break;
+	case 2: cout << "Хуже среднего" << endl; break;
+	case 3: cout << "Средне" << endl; break;
+	case 4: cout << "Хорошо" << endl; break;
+	case 5: cout << "Отлично" << endl; break;
+	default: { cerr << "Ошибка: максимальная оценка - 5" << endl;
+		return 1;
+	}
+	}
+}
